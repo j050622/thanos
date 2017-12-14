a3f18be4c0edd3d14c2bc4381309209e763c2cf0
Mark <guixu2010@gmail.com>
2017-12-14 20:14:40 +0800

    注册app，为thanos应用添加ready方法

--- thanos/apps.py
+++ thanos/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
+from django.utils.module_loading import autodiscover_modules
 
 
 class ThanosConfig(AppConfig):
     name = 'thanos'
+
+    def ready(self):
+        autodiscover_modules('thanos')
