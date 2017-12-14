a98946bb5638e5ddbf3b4d1abc3f2dfddb2b58a2
Mark <guixu2010@gmail.com>
2017-12-14 20:32:50 +0800

    在app01中导入thanos.service.crm.py，并对类名进行注册

--- app01/thanos.py
+++ app01/thanos.py
@@ -1,1 +1,7 @@
-print('--------')
+from django.utils.safestring import mark_safe
+
+from thanos.service import crm
+from app01 import models
+
+crm.site.register(models.Role)
+crm.site.register(models.UserInfo)
