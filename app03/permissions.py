ecbc1cd204991a7d152187ff7a6bcca132c1b354
Mark <guixu2010@gmail.com>
2018-01-03 22:45:05 +0800

    CustomerConfig单独放在了一个文件里

---
+++ app03/permissions.py
@@ -0,0 +1,43 @@
+"""
+configs中的Config类在继承CrmConfig之前，可以从本模块继承一些类，用于自定义权限，将粒度
+控制到按钮
+"""
+
+
+class BasePermission(crm.CrmConfig):
+    """
+    判断是否显示添加、编辑、删除等按钮
+    """
+
+    def get_show_add_btn(self):
+        """判断是否显示添加按钮"""
+
+        codes_list = self.request.session.get(settings.PERM_CODES_LIST)
+        if 'add' not in codes_list:
+            return False
+        return self.show_add_btn
+
+    def get_list_editable(self):
+        """判断是否可以进行编辑"""
+
+        codes_list = self.request.session.get(settings.PERM_CODES_LIST)
+        if 'change' not in codes_list:
+            return []
+        result = []
+        if self.list_editable:
+            result.extend(self.list_editable)
+        return result
+
+    def get_list_display(self):
+        """判断是否显示删除按钮"""
+
+        codes_list = self.request.session.get(settings.PERM_CODES_LIST)
+        result = []
+        if self.list_display:
+            result.extend(self.list_display)
+
+            result.insert(0, crm.CrmConfig.checkbox)
+            if 'delete' in codes_list:
+                result.append(crm.CrmConfig.ele_delete)
+
+        return result
--- app03/thanos.py
+++ app03/thanos.py
@@ -2,14 +2,15 @@
 在crm中注册表
 """
 from thanos.service import crm
-from . import models, configs
+from . import models
+from .configs import configs, customer_config
 
 crm.site.register(models.Department, configs.DepartmentConfig)
 crm.site.register(models.UserInfo, configs.UserInfoConfig)
 crm.site.register(models.School, configs.SchoolConfig)
 crm.site.register(models.Course, configs.CourseConfig)
 crm.site.register(models.ClassList, configs.ClassListConfig)
-crm.site.register(models.Customer, configs.CustomerConfig)
+crm.site.register(models.Customer, customer_config.CustomerConfig)  # 注意路径
 crm.site.register(models.CustomerDistribution, configs.CustomerDistributionConfig)
 crm.site.register(models.ConsultantWeight, configs.ConsultantWeightConfig)
 crm.site.register(models.ConsultRecord, configs.ConsultRecordConfig)
